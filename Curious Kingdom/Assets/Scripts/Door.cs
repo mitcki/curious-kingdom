3fd0a875c5b7f7cf61a81ebdb299b8c8531f53aa
Mitchell Cichocki <mitchell.cichocki@gmail.com>
2021-01-28 19:47:20 -0500

    add tower game parts

---
+++ Curious Kingdom/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) || Input.touchCount == 1)
+        {
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(mousePos))
+            {
+                GetComponent<HingeJoint2D>().useMotor = false;
+            }
+            else
+            {
+                GetComponent<HingeJoint2D>().useMotor = true;
+            }
+        }
+    }
+}

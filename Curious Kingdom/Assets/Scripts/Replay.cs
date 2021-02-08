2260a2ecfe8fd390b0d0a8c0aa76ff4532263fd3
Mitchell Cichocki <mitchell.cichocki@gmail.com>
2021-02-08 07:37:48 -0500

    add tower level 6

---
+++ Curious Kingdom/Assets/Scripts/Replay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Replay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) || Input.touchCount == 1)
+            {
+                if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(mousePos))
+                {
+                    Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+                }
+            }
+        
+    }
+}
---
+++ Curious Kingdom/Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "GoodItem"){
+            Debug.Log("Floor");
+            GameObject replay = GameObject.Find("Replay");
+            replay.GetComponent<SpriteRenderer>().enabled = true;
+            replay.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}

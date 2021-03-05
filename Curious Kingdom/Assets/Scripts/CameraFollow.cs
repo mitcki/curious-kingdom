cb1e56c5cc03ffa4d53a902e32827f12a159f296
Mitchell Cichocki <mitchell.cichocki@gmail.com>
2021-03-05 16:31:03 -0500

    start dungeon game

---
+++ Curious Kingdom/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; public float smoothTime = 0.3F; private Vector3 velocity = Vector3.zero;
+    void Start () {
+     transform.position = new Vector3 (target.transform.position.x,
+                                       target.transform.position.y, 
+                                       transform.position.z);
+ }
+ 
+ void Update () {
+     transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+     transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+ }
+}
---
+++ Curious Kingdom/Assets/Scripts/DragFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFollow : MonoBehaviour
+{
+    Vector3 initialPos; Vector3 curPos; float speed = 0.03f;
+    void Update () {
+     if(Input.GetMouseButton(0)){
+         curPos = Input.mousePosition;
+     }
+ }
+ void OnMouseDown() {
+     initialPos = Input.mousePosition;
+ }
+ 
+ void OnMouseDrag() {
+     if(curPos.x != initialPos.x ) 
+     {
+         transform.position = new Vector3(transform.position.x +(curPos.x - initialPos.x) * Time.deltaTime * speed,  transform.position.y +(curPos.y - initialPos.y) * Time.deltaTime * speed, transform.position.z);
+     }
+ }
+}

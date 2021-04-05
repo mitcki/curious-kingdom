290b14a2d7a23cd17b02c58aca234fc7fede240f
Mitchell Cichocki <mitchellcichocki@Mitchells-MacBook-Pro.local>
2021-04-05 13:30:17 -0400

    add FlashTools - remove Harmoney Assets

---
+++ Curious Kingdom/Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager arTrackedImageManager;
+    
+    private void Awake() {
+        arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable() {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable() {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+        
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args){
+        foreach (var trackedImage in args.added){
+            Debug.Log(trackedImage.name);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

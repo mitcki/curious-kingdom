97cacc3eb114f61a957035892c79a8f7224299fb
Mitchell Cichocki <mitchellcichocki@Mitchells-MacBook-Pro.local>
2021-04-06 17:10:28 -0400

    link menu scene to king 1

---
+++ Curious Kingdom/Assets/Scripts/MenuKing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuKing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) || Input.touchCount == 1)
+        {
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(mousePos))
+            {
+                SceneManager.LoadScene("Intro1");
+
+            }
+            else
+            {
+                // GetComponent<HingeJoint2D>().useMotor = true;
+            }
+        }
+        
+    }
+}

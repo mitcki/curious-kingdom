20db9634b83e594de07202fd5e13530d6bdb1ad0
Mitchell Cichocki <mitchellcichocki@Mitchells-MacBook-Pro.local>
2021-04-06 15:52:42 -0400

    add some intro animation

---
+++ Curious Kingdom/Assets/Scripts/IntroCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int startDelay;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(DelayedAnimation());
+    }
+
+    IEnumerator DelayedAnimation ()
+     {
+        yield return new WaitForSeconds(startDelay);
+        animator.Play("CameraTruckRight");
+
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
